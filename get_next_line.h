4433959798904b610e6c225c3e0493e9ba220e4a
Francesco Monfrecola <frmonfre@e4r3p1.42roma.it>
2023-03-03 08:50:07 +0100

    final

--- get_next_line.h
+++ get_next_line.h
@@ -1,41 +1,56 @@
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frmonfre <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 11:25:09 by frmonfre          #+#    #+#             */
+/*   Updated: 2023/03/03 08:40:21 by frmonfre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
 
 typedef struct s_elem
 {
-    char            *cont;
-    struct s_elem   *next;
-    struct s_elem   *prev;
-}   t_elem;
+	char			*cont;
+	struct s_elem	*next;
+	struct s_elem	*prev;
+}	t_elem;
 
 struct s_lst
 {
-    t_elem  *head;
-    t_elem  *tail;
-    int     chrs;
-    int     nwln;
-    int     buff;
+	t_elem	*head;
+	t_elem	*tail;
+	int		chrs;
+	int		nwln;
+	int		buff;
 };
 
-typedef struct s_lst   *t_lst; 
+typedef struct s_lst	*t_lst;
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 
-void    extract_line(t_lst stash, char **line);
+void	extract_line(t_lst stash, char **line);
 
-void    clean_stash(t_lst stash);
+void	clean_stash(t_lst stash);
 
-void    add_stash(t_lst stash, char *buf);
+void	add_stash(t_lst stash, char *buf);
 
-void    read_stash(int fd, t_lst stash);
+int		read_stash(int fd, t_lst stash);
 
-void    free_stash(t_elem *I);
+int		ft_strlen(char *s);
 
-int     ft_strlen(char *s);
+int		fnd_new_line(t_lst stash);
 
-int     fnd_new_line(t_lst stash);
+int		extract_and_free(t_lst stash, char **line);
 
+t_elem	*mng_nwln(t_lst stash);
+
+void	init_stash(t_lst *stash);
 #endif
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,152 +1,138 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frmonfre <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/02 11:28:02 by frmonfre          #+#    #+#             */
+/*   Updated: 2023/03/02 12:06:33 by frmonfre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line_bonus.h"
 
-void    extract_line(t_lst stash, char **line)
+void	extract_line(t_lst stash, char **line)
 {
-    t_elem  *I;                                                     // Iteratore per scorrere la lista
-    int     i;
-    int     ln;
+	int		ln;
 
-    ln = 0;
-    I = stash->head;                                                // Comincio dalla testa della lista
-    *line = (char *) malloc(sizeof(char) * (stash->chrs + 1));      // Alloco una riga di output che abbia il giusto numero di celle
-    if (!*line)
-        return;
-    while (I)                                                       // Scorro la lista finché non termina
-    {
-        i = 0;
-        while (I->cont[i] && I->cont[i] != '\n')                    // Scorro il content finché o non finisce o trovo uno \n
-        {
-            line[0][ln] = I->cont[i];                               // Copio la stringa del content nella riga di output
-            i++;
-            ln++;
-        }
-        if (I->cont[i] == '\n')                                         // Se il while è terminato a causa di uno \n
-        {
-            line[0][ln] = '\n';                                         // Posiziono uno \n nella riga di output
-            ln++;
-            stash->nwln = i + 1;
-        }
-        I = I->next;                                                    // Caratteri della lista - caratteri copiati
-    }
-    stash->buff = stash->chrs - ln;                                 // Il numero di caratteri post \n è uguale a
-    line[0][ln] = '\0';                                                 // Null termino la riga di output
+	if (stash == NULL)
+		return ;
+	*line = (char *) malloc(sizeof(char) * (stash->chrs + 1));
+	if (!*line)
+		return ;
+	ln = extract_and_free(stash, line);
+	stash->head = stash->tail;
+	stash->buff = stash->chrs - ln;
+	line[0][ln] = '\0';
 }
 
-void    clean_stash(t_lst stash)
+void	clean_stash(t_lst stash)
 {
-    int     i;
-    t_elem  *nw;                                                    
+	int		i;
+	t_elem	*nw;
 
-    i = 0;
-    if (stash->nwln)                                                    // Se è presente uno \n e quindi dell'overflow
-    {
-        nw = (t_elem *) malloc(sizeof(t_elem));                         // Creo un nuovo elemento di lista
-        if (!nw)
-            return;
-        nw->cont = (char *) malloc(sizeof(char) * (stash->buff + 1));   // Alloco un content delle dimensioni corrette
-        if (!nw->cont)
-            return;
-        nw->next = NULL;                                                
-        nw->prev = NULL;
-        stash->chrs = stash->buff;                                      // Aggiorno il numero di caratteri nella lista
-        while (stash->tail->cont[stash->nwln + i])                      // Scorro il content dell'ultimo elemento
-        {
-            nw->cont[i] = stash->tail->cont[stash->nwln + i];            // Copio il post \n nel nuovo content
-            i++;                                                        
-        }
-        nw->cont[i] = '\0';                                             // Null termino il nuovo content
-    }
-    free_stash(stash->head);                                            // Dealloco l'intera lista
-    if (stash->nwln)
-    {
-        stash->head = nw;                                                   // nw è ora l'unico elemento della lista
-        stash->tail = nw;
-    }
-    else
-    {
-        stash->head = NULL;
-        stash->tail = NULL;
-        stash->chrs = 0;
-    }
-    stash->nwln = 0;                                                    // Azzero il flag new_line
+	if (stash == NULL)
+		return ;
+	i = 0;
+	if (stash->nwln)
+		nw = mng_nwln(stash);
+	if (stash->tail != NULL)
+	{
+		free(stash->tail->cont);
+		free(stash->tail);
+	}
+	if (stash->nwln)
+	{
+		stash->head = nw;
+		stash->tail = nw;
+	}
+	else
+	{
+		stash->head = NULL;
+		stash->tail = NULL;
+		stash->chrs = 0;
+	}
+	stash->nwln = 0;
 }
 
-void    add_stash(t_lst stash, char *buf)
+void	add_stash(t_lst stash, char *buf)
 {
-    int i;
-    t_elem  *nw;
+	int		i;
+	t_elem	*nw;
 
-
-    nw = (t_elem *) malloc(sizeof(t_elem));                           // Alloca un nuovo elemento della lista
-    if (!nw)    
-        return;
-    nw->cont = (char *) malloc(sizeof(char) * (ft_strlen(buf) + 1));  // Alloca una stringa nel content della lista
-    if (!nw->cont)
-        return;
-    i = 0;
-    while (buf[i])                                                    // Scorri la stringa del buffer
-    {   
-        nw->cont[i] = buf[i];                                         // Copia il carattere nel content della lista
-        i++;
-        stash->chrs++;                                                // Incrementa il contatore di caratteri della lista
-    }
-    nw->cont[i] = '\0';                                               // Null termina il nuovo content
-    nw->next = NULL;                                                  // Inizializza i valori dell'elemento di lista
-    nw->prev = stash->tail;                                           // Il precedente di nw è il vecchio ultimo elemento
-    if (stash->tail != NULL)                                          // Se la lista non è vuota
-        stash->tail->next = nw;                                       // Il successivo del vecchio ultimo diventa nw
-    stash->tail = nw;                                                 // Ora nw è l'ultimo
-    if (stash->head == NULL)                                          // Se la lista è vuota
-        stash->head = nw;                                             // Ora nw è anche il primo elemento della lista
+	nw = (t_elem *) malloc(sizeof(t_elem));
+	if (!nw)
+		return ;
+	nw->cont = (char *) malloc(sizeof(char) * (ft_strlen(buf) + 1));
+	if (!nw->cont)
+		return ;
+	i = 0;
+	while (buf[i])
+	{
+		nw->cont[i] = buf[i];
+		i++;
+		stash->chrs++;
+	}
+	nw->cont[i] = '\0';
+	nw->next = NULL;
+	nw->prev = stash->tail;
+	if (stash->tail != NULL)
+		stash->tail->next = nw;
+	stash->tail = nw;
+	if (stash->head == NULL)
+		stash->head = nw;
 }
 
-void    read_stash(int fd, t_lst stash)                         
+int	read_stash(int fd, t_lst stash)
 {
-    char    *buf;
-    int     byte;
+	char	*buf;
+	int		byte;
 
-    byte = 1;
-    while (!fnd_new_line(stash) && byte > 0)                    // Scorri il file finché non hai trovato uno \n o un EOF
-    {
-        buf = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
-        if (!buf)                                               
-            return;
-        byte = (int) read(fd, buf, BUFFER_SIZE);                // Conta i bytes letti
-        if (byte == -1)
-        {
-            free(buf);
-            return;
-        }
-        buf[byte] = '\0';                                       // NUll termina la stringa appena inserita nel buffer
-        add_stash(stash, buf);                                  // Aggiungi la stringa del buffer nella lista
-        free(buf);                                              // Dealloca il buffer
-    }
+	byte = 1;
+	buf = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (!fnd_new_line(stash) && byte > 0)
+	{
+		if (!buf)
+			return (1);
+		byte = (int) read(fd, buf, BUFFER_SIZE);
+		if (byte <= 0)
+		{
+			free(buf);
+			return (1);
+		}
+		buf[byte] = '\0';
+		add_stash(stash, buf);
+	}
+	free(buf);
+	return (0);
 }
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static t_lst  files[1024];
-    t_lst         stash;
-    char          *line;                                        // Riga di output
-    int           tmp;
+	static t_lst	files[1024];
+	t_lst			stash;
+	char			*line;
+	int				end;
 
-    if (fd < 0 || BUFFER_SIZE < 1 || read(fd, &line, 0) < 0)    // Error managment
-        return (NULL);
-
-    tmp = (files[fd] != NULL);
-
-    if (tmp)
-        stash = files[fd];
-    else
-        stash = nw_t_lst(fd, files);
-
-    read_stash(fd, stash);                                      // Leggi e posiziona nella linked list le stringhe di buffer
-    extract_line(stash, &line);                                 // Estrai la riga di output
-    clean_stash(stash);                                         // Dealloca la lista, eventualmente mantenendo un elemento di over
-    if (line[0] == '\0')
-    {
-        free(line);
-        return (NULL);
-    }
-    return (line);                                              // Restituisci la riga di output
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, &line, 0) < 0)
+		return (NULL);
+	if (files[fd] != NULL)
+		stash = files[fd];
+	else
+		stash = nw_t_lst(fd, files);
+	end = read_stash(fd, stash);
+	extract_line(stash, &line);
+	clean_stash(stash);
+	if (end)
+	{
+		free(files[fd]);
+		files[fd] = NULL;
+	}
+	if (!line[0])
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
 }
